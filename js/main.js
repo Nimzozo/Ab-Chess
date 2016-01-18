53564af55e12630a3f2991a8c52e5ead41821233
A. B <Nimzozo@users.noreply.github.com>
2016-01-18 20:57:01 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 window.addEventListener('load', function () {
-    var test = abChess('abc', 400);
-    test.draw();
-    test.fen.set();
-
-    var flipHandler = function () {
-        test.flip();
-    };
-    document.getElementById('flipButton').addEventListener('click', flipHandler);
+    var abc = abChess('abc', 400);
+    var fenInput = document.getElementById('fenInput');
+    abc.draw();
+    document.getElementById('startButton').addEventListener('click', function () {
+        abc.fen.set();
+        fenInput.value = abc.fen.get();
+    });
+    document.getElementById('emptyButton').addEventListener('click', function () {
+        abc.fen.set('8/8/8/8/8/8/8/8');
+        fenInput.value = abc.fen.get();
+    });
+    document.getElementById('flipButton').addEventListener('click', function () {
+        abc.flip();
+    });
+    fenInput.addEventListener('change', function (e) {
+        abc.fen.set(e.target.value);
+    });
 
 });
