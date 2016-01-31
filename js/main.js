6a73fb2b329ca1cd897a2ad012e5bde67df4f6ff
A. B <Nimzozo@users.noreply.github.com>
2016-01-31 19:16:03 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', function () {
-    var abc = new AbChess('abc', 400);
+    var abc = new AbChess('abc', 350);
     var fenInput = document.getElementById('fenInput');
+    var lastMoveInfo = document.getElementById('lastMoveInfo');
+    var moveInput = document.getElementById('moveInput');
+    var testList = document.getElementById('testList');
+    var turnInfo = document.getElementById('turnInfo');
+    var checkInfo = document.getElementById('checkInfo');
     abc.draw();
     document.getElementById('startButton').addEventListener('click', function () {
         abc.fen.set();
@@ -13,10 +18,25 @@ window.addEventListener('load', function () {
     document.getElementById('flipButton').addEventListener('click', function () {
         abc.flip();
     });
-    fenInput.addEventListener('change', function (e) {
-        abc.fen.set(e.target.value);
+    fenInput.addEventListener('change', function () {
+        abc.fen.set(fenInput.value);
     });
     document.getElementById('abc').addEventListener('dragend', function () {
         fenInput.value = abc.fen.get();
+    });
+    document.getElementById('moveButton').addEventListener('click', function () {
+        var inputMove = moveInput.value;
+        var isLegal = abc.game.isLegal(inputMove);
+        if (isLegal) {
+            fenInput.value = abc.game.play(inputMove);
+        }
+        lastMoveInfo.innerText = (abc.game.getActiveColor() === 'w')
+            ? 'White to move'
+            : 'Black to move';
+        checkInfo.innerText = (abc.game.isInCheck())
+            ? 'Check !'
+            : '';
+        abc.fen.set(fenInput.value);
+        tester('testList', isLegal, inputMove + ' is legal.');
     });
 });
