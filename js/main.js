ed7f831f70e0b072184e99748cb914058ca769c8
A. B <Nimzozo@users.noreply.github.com>
2016-02-03 01:19:41 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,43 @@
 window.addEventListener('load', function () {
-    var abc = new AbChess('abc', 350);
+    var config = {};
+    var abc = new AbChess('abc', config);
     var fenInput = document.getElementById('fenInput');
     var lastMoveInfo = document.getElementById('lastMoveInfo');
     var moveInput = document.getElementById('moveInput');
-    var testList = document.getElementById('testList');
     var turnInfo = document.getElementById('turnInfo');
     var checkInfo = document.getElementById('checkInfo');
-    abc.draw();
+    abc.board.draw();
+    abc.board.fen.set();
+    updateInfo();
     document.getElementById('startButton').addEventListener('click', function () {
-        abc.fen.set();
-        fenInput.value = abc.fen.get();
+        abc.board.fen.set();
+        fenInput.value = abc.board.fen.get();
     });
     document.getElementById('emptyButton').addEventListener('click', function () {
-        abc.fen.set('8/8/8/8/8/8/8/8');
-        fenInput.value = abc.fen.get();
+        abc.board.fen.set('8/8/8/8/8/8/8/8');
+        fenInput.value = abc.board.fen.get();
     });
     document.getElementById('flipButton').addEventListener('click', function () {
-        abc.flip();
-    });
-    fenInput.addEventListener('change', function () {
-        abc.fen.set(fenInput.value);
+        abc.board.flip();
     });
     document.getElementById('abc').addEventListener('drop', function () {
-        fenInput.value = abc.fen.get();
+        fenInput.value = abc.game.getFEN();
+        updateInfo();
     });
     document.getElementById('moveButton').addEventListener('click', function () {
         var inputMove = moveInput.value;
         var isLegal = abc.game.isLegal(inputMove);
         if (isLegal) {
             fenInput.value = abc.game.play(inputMove);
+            updateInfo();
         }
-        lastMoveInfo.innerText = (abc.game.getActiveColor() === 'w')
-            ? 'White to move'
-            : 'Black to move';
+    });
+    function updateInfo() {
+        turnInfo.innerText = (abc.game.getActiveColor() === 'w')
+            ? 'White to move.'
+            : 'Black to move.';
         checkInfo.innerText = (abc.game.isInCheck())
             ? 'Check !'
             : '';
-        abc.fen.set(fenInput.value);
-        tester('testList', isLegal, inputMove + ' is legal.');
-    });
+    }
 });
