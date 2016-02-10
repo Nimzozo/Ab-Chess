1adf92fd8a245a548272abdac2f113a6b5917be1
A. B <Nimzozo@users.noreply.github.com>
2016-02-10 14:31:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,32 +3,39 @@ window.addEventListener('load', function () {
     var abChess = new AbChess('abc');
     var checkInfo = document.getElementById('checkInfo');
     var container = document.getElementById('abc');
-    var emptyButton = document.getElementById('emptyButton');
     var fenInput = document.getElementById('fenInput');
     var flipButton = document.getElementById('flipButton');
     var lastMoveInfo = document.getElementById('lastMoveInfo');
-    var moveButton = document.getElementById('moveButton');
-    var moveInput = document.getElementById('moveInput');
-    var startButton = document.getElementById('startButton');
+    var pgnButton = document.getElementById('pgnButton');
+    var pgnTextarea = document.getElementById('pgn');
     var turnInfo = document.getElementById('turnInfo');
     function updateInfo() {
+        var lastIndex = 0;
+        var lastMove = '';
+        var moves = [];
+        var pgn = abChess.getPGN(true);
+     //   pgnDiv.innerText = pgn;
+        moves = abChess.getMoves();
+        lastIndex = moves.length - 1;
+        if (lastIndex >= 0) {
+            lastMove = moves[lastIndex];
+        }
         fenInput.value = abChess.getFEN();
-        checkInfo.innerText = (abChess.isInCheck())
-            ? 'Check !'
-            : '';
-        lastMoveInfo.innerText = 'Last move : ';
+        checkInfo.innerText = '';
+        if (abChess.isInCheck()) {
+            if (abChess.isCheckmate()) {
+                checkInfo.innerText = 'Checkmate !';
+            } else {
+                checkInfo.innerText = 'Check !';
+            }
+        } else if (abChess.isStalemate()) {
+                checkInfo.innerText = 'Stalemate !';
+        }
+        lastMoveInfo.innerText = 'Last move : ' + lastMove;
         turnInfo.innerText = (abChess.getActiveColor() === 'w')
             ? 'White to move.'
             : 'Black to move.';
     }
-    startButton.addEventListener('click', function () {
-        abChess.setFEN();
-        requestAnimationFrame(updateInfo);
-    });
-    emptyButton.addEventListener('click', function () {
-        abChess.setFEN('8/8/8/8/8/8/8/8');
-        requestAnimationFrame(updateInfo);
-    });
     flipButton.addEventListener('click', function () {
         abChess.flip();
     });
@@ -38,12 +45,9 @@ window.addEventListener('load', function () {
     container.addEventListener('dragend', function () {
         requestAnimationFrame(updateInfo);
     });
-    moveButton.addEventListener('click', function () {
-        var inputMove = moveInput.value;
-        var isLegal = abChess.isLegal(inputMove);
-        if (isLegal) {
-            requestAnimationFrame(updateInfo);
-        }
+    pgnButton.addEventListener('click', function () {
+        var pgn = pgnTextarea.value;
+        abChess.isValidPGN(pgn);
     });
     abChess.draw();
     abChess.setFEN();
