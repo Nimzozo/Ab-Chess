b10b872db58ba972d15e8f2aacd3a41d1133bb5d
A. B <fischerbobby@msn.com>
2017-01-19 22:28:48 +0100

    html accordeon init

--- javascripts/accordeon.js
+++ javascripts/accordeon.js
@@ -1,21 +1,21 @@
 window.addEventListener("load", function () {
     "use strict";
 
-    var activeClassName = "sub-list_active";
     var activeHeaderClassName = "list__header_active";
-    var activeHeader;
-    var activeList;
-    var defaultClassName = "sub-list";
+    var activeListClassName = "sub-list_active";
+    var activeHeader = document.getElementsByClassName(activeHeaderClassName)[0];
+    var activeList = document.getElementsByClassName(activeListClassName)[0];
     var defaultHeaderClassName = "list__header";
-    var lists = document.getElementsByClassName(defaultClassName);
+    var defaultListClassName = "sub-list";
+    var lists = document.getElementsByClassName(defaultListClassName);
 
     function changeHeight(element, step, limit) {
         var clientHeight = element.clientHeight;
         var newHeight = clientHeight + step;
         var strHeight = newHeight + "px";
         element.style.height = strHeight;
-        if ((step > 0 && clientHeight < limit) ||
-            (step < 0 && clientHeight > limit)) {
+        if ((step > 0 && newHeight < limit) ||
+            (step < 0 && newHeight > limit)) {
             requestAnimationFrame(function () {
                 changeHeight(element, step, limit);
             });
@@ -39,20 +39,22 @@ window.addEventListener("load", function () {
     function closeList(header, list) {
         var items = getItemsFromList(list);
         items.forEach(function (item) {
+            item.style.display = "block";
             changeHeight(item, -4, 0);
         });
         header.className = defaultHeaderClassName;
+        list.className = defaultListClassName;
         activeList = undefined;
     }
 
     function openList(header, list) {
         var items = getItemsFromList(list);
         header.className = activeHeaderClassName;
-        list.className = activeClassName;
+        list.className = activeListClassName;
         items.forEach(function (item) {
             item.style.height = 0;
             item.style.display = "block";
-            changeHeight(item, 4, 32);
+            changeHeight(item, 4, 36);
         });
         if (activeList !== undefined) {
             closeList(activeHeader, activeList);
@@ -62,9 +64,8 @@ window.addEventListener("load", function () {
     }
 
     Object.keys(lists).forEach(function (key) {
-        var listHeader;
         var list = lists[key];
-        listHeader = list.firstElementChild;
+        var listHeader = list.firstElementChild;
         listHeader.addEventListener("click", function () {
             if (list === activeList) {
                 closeList(listHeader, list);
