5b2ce9673c954aa2ad08b8200b2eac8540d22cd0
A. B <fischerbobby@msn.com>
2017-03-09 18:43:54 +0100

    test.html

--- test/test.js
+++ test/test.js
@@ -2,21 +2,23 @@ window.addEventListener("load", function () {
     "use strict";
 
     var abChess = {};
+    var currentIndex = 0;
     var firstButton = document.getElementById("firstButton");
     var flipButton = document.getElementById("flipButton");
-    var index = 0;
     var lastButton = document.getElementById("lastButton");
+    var lastIndex = 0;
+    var moves = [];
+    var movesDiv = document.getElementById("moves-div");
     var nextButton = document.getElementById("nextButton");
     var options = {
-        animationSpeed: "normal",
+        animationSpeed: "slow",
         clickable: true,
         draggable: true,
         imagesPath: "../src/images/wikipedia/"
     };
     var pgnButton = document.getElementById("pgn-button");
-    var pgnNotation = document.getElementById("pgn-notation");
-    var pgnSpanClass = "pgn-move";
-    var pgnText = document.getElementById("pgn-text");
+    var pgnSpanClass = "move-span";
+    var pgnSelectedSpanId = "move-span_selected";
     var pgnTextArea = document.getElementById("pgn-textarea");
     var previousButton = document.getElementById("previousButton");
     var resetButton = document.getElementById("resetButton");
@@ -26,76 +28,59 @@ window.addEventListener("load", function () {
     abChess.setFEN();
 
     function navigate(index) {
-
-        // Navigate on the desired move index.
-
-        abChess.navigate(index);
+        var selectedSpan = document.getElementById(pgnSelectedSpanId);
+        var spans = document.getElementsByClassName(pgnSpanClass);
+        if (index < 0 || index > lastIndex) {
+            return;
+        }
+        currentIndex = index;
+        abChess.navigate(currentIndex);
+        if (selectedSpan !== null) {
+            selectedSpan.removeAttribute("id");
+        }
+        if (index > 0 && spans.length > 0) {
+            spans[index - 1].id = pgnSelectedSpanId;
+        }
     }
-
-    function addPGNMove(pgnMove, moveIndex) {
-
-        // Update the PGN by adding a move.
-
-        var fullmoveNumber = "";
-        var moveSpan = {};
-        var textNode = {};
-        fullmoveNumber = (moveIndex % 2 === 0)
-            ? " " + (moveIndex / 2 + 1) + ". "
-            : " ";
-        textNode = document.createTextNode(fullmoveNumber);
-        pgnNotation.appendChild(textNode);
-        moveSpan = document.createElement("SPAN");
-        moveSpan.className = pgnSpanClass;
-        moveSpan.innerText = pgnMove;
-        moveSpan.addEventListener("click", function () {
-            navigate(moveIndex + 1);
+    function addMoveSpan(move, i) {
+        var span = document.createElement("SPAN");
+        span.className = pgnSpanClass;
+        span.innerHTML = move;
+        span.addEventListener("click", function () {
+            navigate(i + 1);
         });
-        pgnNotation.appendChild(moveSpan);
+        movesDiv.appendChild(span);
     }
-
-    abChess.onMovePlayed(function () {
-        var lastIndex = 0;
-        var lastMove = "";
-        var pgnMoves = abChess.getGameMovesPGN();
-        lastIndex = pgnMoves.length - 1;
-        lastMove = pgnMoves[lastIndex];
-        addPGNMove(lastMove, lastIndex);
-        pgnText.innerText = abChess.getFEN(lastIndex + 1);
-    });
-
-    pgnButton.addEventListener("click", function () {
-        abChess.reset();
+    function clearSpans() {
+        while (movesDiv.hasChildNodes()) {
+            movesDiv.removeChild(movesDiv.lastElementChild);
+        }
+    }
+    function importPGN() {
         abChess.setPGN(pgnTextArea.value);
-        index = abChess.getLastPositionIndex();
-        abChess.navigate(index);
-    });
-
+        clearSpans();
+        moves = abChess.getGameMovesPGN();
+        moves.forEach(addMoveSpan);
+        lastIndex = abChess.getLastPositionIndex();
+        navigate(lastIndex);
+    }
+    pgnButton.addEventListener("click", importPGN);
     resetButton.addEventListener("click", function () {
         abChess.reset();
+        clearSpans();
     });
-
-    flipButton.addEventListener("click", function () {
-        abChess.flip();
-    });
-
+    flipButton.addEventListener("click", abChess.flip);
     firstButton.addEventListener("click", function () {
-        index = 0;
-        abChess.navigate(index);
+        navigate(0);
     });
-
     previousButton.addEventListener("click", function () {
-        index -= 1;
-        abChess.navigate(index);
+        navigate(currentIndex - 1);
     });
-
     nextButton.addEventListener("click", function () {
-        index += 1;
-        abChess.navigate(index);
+        navigate(currentIndex + 1);
     });
-
     lastButton.addEventListener("click", function () {
-        index = abChess.getLastPositionIndex();
-        abChess.navigate(index);
+        navigate(lastIndex);
     });
 
 });
