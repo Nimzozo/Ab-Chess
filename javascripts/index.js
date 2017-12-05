08a89096913129ed4e32de5975d857c3daaf7de6
A. B <Nimzozo@users.noreply.github.com>
2017-12-05 20:55:25 +0100

    home

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,28 @@
 window.addEventListener("load", function () {
     "use strict";
 
-    var abChess = new AbChess("chessboard");
-    abChess.setFEN();
+    var abChess = {};
+    var index = 0;
+    var lastIndex = 0;
+    var options = {
+        clickable: false,
+        draggable: false,
+        width: 480
+    }
+    var gameCode = document.getElementById("game-code");
+    var pgn = gameCode.innerText;
+    abChess = new AbChess("chessboard", options)
+    abChess.setPGN(pgn);
+    lastIndex = abChess.getMovesPGN().length;
+
+    (function viewNext() {
+        if (index > lastIndex) {
+            return;
+        }
+        abChess.view(index);
+        index += 1;
+        requestAnimationFrame(function () {
+            setTimeout(viewNext, 2000);
+        });
+    }())
 });
